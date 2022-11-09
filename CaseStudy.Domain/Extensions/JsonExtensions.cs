d61eb7ff14288d7eb85b479fedf8cea9ddb49e15
yoheswaran.n <yoheswaran.n@spurtreetech.in>
2022-11-09 18:24:29 +0530

    Added json extension

---
+++ CaseStudy.Domain/Extensions/JsonExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CaseStudy.Domain.Extensions
+{
+    public static class JsonExtensions
+    {
+        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static T? FromJson<T>(this string json) =>
+            JsonSerializer.Deserialize<T>(json, _jsonOptions);
+
+        public static string ToJson<T>(this T obj) =>
+            JsonSerializer.Serialize<T>(obj, _jsonOptions);
+    }
+}
